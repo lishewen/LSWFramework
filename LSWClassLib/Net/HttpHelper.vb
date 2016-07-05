08092dc4485534a7f5825b991a9b83ffd2d28f68
算神 <lishewen@gmail.com>
2016-07-05 10:15:44 +0800

    增加GZIP解压缩

--- LSWClassLib/Net/HttpHelper.vb
+++ LSWClassLib/Net/HttpHelper.vb
@@ -1,34 +1,43 @@
 ﻿Imports System.Threading.Tasks
 Imports System.Net.Http
 Imports System.Text
+Imports System.Net
 
 Namespace Net
-    Public Module HttpHelper
-        Public Async Function GetHtmlAsync(url As String) As Task(Of String)
-            Try
-                Dim h As New System.Net.Http.HttpClient
-                Dim req = Await h.GetAsync(New Uri(url, UriKind.RelativeOrAbsolute))
-                req.EnsureSuccessStatusCode()
-                Dim txt = Await req.Content.ReadAsStringAsync
-                Return txt
-            Catch ex As Exception
-                Debug.WriteLine(ex.Message)
-                Return String.Empty
-            End Try
-        End Function
+	Public Module HttpHelper
+		Public Async Function GetHtmlAsync(url As String) As Task(Of String)
+			Try
+				Dim handler As New HttpClientHandler() With
+					{
+						.AutomaticDecompression = DecompressionMethods.GZip Or DecompressionMethods.Deflate
+					}
+				Dim h As New Http.HttpClient(handler)
+				Dim req = Await h.GetAsync(New Uri(url, UriKind.RelativeOrAbsolute))
+				req.EnsureSuccessStatusCode()
+				Dim txt = Await req.Content.ReadAsStringAsync
+				Return txt
+			Catch ex As Exception
+				Debug.WriteLine(ex.Message)
+				Return String.Empty
+			End Try
+		End Function
 
-        Public Async Function GetHtmlAsync(url As String, ucoid As String) As Task(Of String)
-            Try
-                Dim h As New System.Net.Http.HttpClient
-                Dim req = Await h.GetAsync(New Uri(url, UriKind.RelativeOrAbsolute))
-                req.EnsureSuccessStatusCode()
-                Dim bt = Await req.Content.ReadAsByteArrayAsync
-                Dim txt = Encoding.GetEncoding(ucoid).GetString(bt)
-                Return txt
-            Catch ex As Exception
-                Debug.WriteLine(ex.Message)
-                Return String.Empty
-            End Try
-        End Function
-    End Module
+		Public Async Function GetHtmlAsync(url As String, ucoid As String) As Task(Of String)
+			Try
+				Dim handler As New HttpClientHandler() With
+					{
+						.AutomaticDecompression = DecompressionMethods.GZip Or DecompressionMethods.Deflate
+					}
+				Dim h As New Http.HttpClient(handler)
+				Dim req = Await h.GetAsync(New Uri(url, UriKind.RelativeOrAbsolute))
+				req.EnsureSuccessStatusCode()
+				Dim bt = Await req.Content.ReadAsByteArrayAsync
+				Dim txt = Encoding.GetEncoding(ucoid).GetString(bt)
+				Return txt
+			Catch ex As Exception
+				Debug.WriteLine(ex.Message)
+				Return String.Empty
+			End Try
+		End Function
+	End Module
 End Namespace
